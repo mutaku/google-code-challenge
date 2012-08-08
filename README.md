b50bf1a47fe679719a28b893534b103387a71942
Matthew Martz <xiao_haozi@mutaku.com>
2012-08-08 21:06:55 +0100

    minimum scalar product problem added

--- README.md
+++ README.md
@@ -3,6 +3,10 @@ google-code-challenge
 
 meanderings in the google code challenge practice sets
 
-- Store Credit Problem
-- Reverse Words Problem
-- T9 Spelling Problem
+
+Problems
+
+- Store Credit
+- Reverse Words
+- T9 Spelling
+- Minimum Scalar Product
---
+++ minimum_scalar_product/min_scalar.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# Google Code Challenge - T9 Spelling Problem
+# http://code.google.com/codejam/contest/32016/dashboard#s=p0
+
+import itertools
+from time import time
+
+
+def scalar(v1, v2):
+    return sum(map(lambda x, y: x * y, sorted(v1), sorted(v2, reverse=True)))
+
+def grouper(n, iterable, fillvalue=None):
+    args = [iter(iterable)] * n
+    return itertools.izip_longest(fillvalue=fillvalue, *args)
+
+def makeintstring(s):
+    return [int(x) for x in s.split(' ')]
+
+def generator(data):
+    results = []
+    for num, v1string, v2string in grouper(3, data):
+        v1, v2 = makeintstring(v1string), makeintstring(v2string)
+        results.append(scalar(v1, v2))
+    return results
+
+def run(data_file, output_file):
+    data = open(data_file)
+    output = open(output_file, 'w')
+    data.next()
+
+    start = time()
+    results = generator(data)
+    end = time()
+    complete = end - start
+
+    for run, result in enumerate(results, start=1):
+        resultstring = "Case #%d: %d \n" % (run, result)
+        output.write(resultstring)
+
+    data.close()
+    output.close()
+    print "Computed %s and wrote %s taking %f s" % (data_file, output_file, complete)
+
+if __name__ == "__main__":
+    f = raw_input("File location: ")
+    run(f, f.replace(".in", ".out"))
